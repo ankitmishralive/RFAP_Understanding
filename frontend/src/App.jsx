2e806761112f977df5f7a6f3d15b4cb990302d00
ankitxtowod@gmail.com <ankitmishralive@ankitmishra.ai>
2024-03-02 04:00:24 +0530

    remaining prac for tmmrw

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,27 +2,54 @@ import { useState,useEffect } from 'react'
 import './App.css'
 
 import ContactList from './ContactList'
+import ContactForm from './contactForm'
 
 function App() {
 
-  const [contacts,setContacts] = useState([{"firstName":"yo","lastName":"yes","email":"email",id:1}])
+  const [contacts,setContacts] = useState([{"firstName":"yo","lastName":"yes","email":"email","id":1}])
+  const [isModalOpen,setIsModalOpen] = useState(false)
 
   useEffect(()=>{
-    // fetchContacts()
+    fetchContacts()
   },[])
 
   const fetchContacts = async () =>{
       const response = await fetch("http://127.0.0.1:5000/contacts")
       const data = await response.json() 
-      setContacts(data.contacts)
-      console.log(data.contacts)
+      setContacts(data.Contacts)
+      console.log("Data here :",data.Contacts)
+  }
+
+  const openModal =()=>{
+  if(!isModalOpen)  setIsModalOpen(true)
+
+  }
+
+  const closeModal =()=>{
+       setIsModalOpen(false)
   }
 
 
 
   return (
 
-    <ContactList contacts={contacts}/>
+
+
+    <>
+    <ContactList contacts={contacts} />
+    <button onClick={openModal}>Create New Contact</button>
+    {isModalOpen&& <div className='modal'>
+      
+      <div className='modal-content'>
+             
+       <span className='close' onClick={closeModal}> &times;</span>
+       <ContactForm />
+      </div>
+      </div>
+      
+      }
+</>
+
   )
 }
 
--- frontend/src/ContactList.jsx
+++ frontend/src/ContactList.jsx
@@ -17,19 +17,18 @@ const ContactList = ({contacts}) => {
                 </tr>
             </thead>
             <tbody>
-          {contacts.map((contact) => (
-            <tr key={contact.id}>
-         
-              <td>{contact.firstName}</td> {/* Assuming firstName property */}
-              <td>{contact.lastName}</td> {/* Assuming lastName property */}
-              <td>{contact.email}</td>
-              <td>
-                <button>Update</button>
-                <button>Delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+                { contacts?.map((contact) => (
+                    <tr key={contact.id}>
+                        <td>{contact.firstName}</td>
+                        <td>{contact.lastName}</td>
+                        <td>{contact.email}</td>
+                        <td>
+                            <button onClick={() => updateContact(contact)}>Update</button>
+                            <button onClick={() => onDelete(contact.id)}>Delete</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
            
         </table>
 
